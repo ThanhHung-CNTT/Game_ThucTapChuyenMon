791bae0cfe9f7425cf5f1d762e6e3d1084c28bf9
Nguyễn Phạm Thành Hưng <thanhhung16092002@gmail.com>
2023-06-01 17:29:03 +0700

    create animation

--- MasterChef/Assets/Scripts/Player.cs
+++ MasterChef/Assets/Scripts/Player.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
-    // Update is called once per frame
+
+    private bool isWalking;
     private void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
@@ -30,7 +31,14 @@ public class Player : MonoBehaviour
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        isWalking = moveDir != Vector3.zero;
         float rotataSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotataSpeed);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
