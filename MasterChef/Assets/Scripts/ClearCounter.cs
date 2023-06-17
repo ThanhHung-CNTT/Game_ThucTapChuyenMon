6a90e6d2b3f30df84a3861c3b65cb547103bbf1d
Nguyễn Phạm Thành Hưng <thanhhung16092002@gmail.com>
2023-06-17 22:00:38 +0700

    lay va dat nguyen lieu

--- MasterChef/Assets/Scripts/ClearCounter.cs
+++ MasterChef/Assets/Scripts/ClearCounter.cs
@@ -8,9 +8,33 @@ public class ClearCounter : BaseCounter
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
     
 
-    
     public override void Interact(Player player)
     {
-       
+        if (!HasKitchenObject())
+        {
+            //There is no kitchen object here
+            if (player.HasKitchenObject())
+            {
+                //player carring something
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else
+            {
+                //player not carring anything
+            }
+        }
+        else
+        {
+            //There is not kitchen object here
+            if(player.HasKitchenObject())
+            {
+                //player is carring something
+            }
+            else
+            {
+                //player is not carring anything
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 }
